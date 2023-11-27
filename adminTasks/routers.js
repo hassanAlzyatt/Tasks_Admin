3a8cb8a8ecb138621612f2dbc18c2b2829509b79
hassanAlzyatt <152031047+hassanAlzyatt@users.noreply.github.com>
2023-11-27 15:35:26 +0200

    Update routers.js

--- adminTasks/routers.js
+++ adminTasks/routers.js
@@ -4,10 +4,17 @@ const serves =require('./serves');
 
 
 const router = express.Router();
-
+// add ,update,delete in menue items
 router.post('/admin/addProduct',serves.addProduct);
 router.put('/admin/updateItem/:itemId', serves.updateRecord)
 router.delete('/admin/deleteMenuItem/:itemId', serves.deleteRecord)
 
+// Stock Routes 
+router.get('/menuitems/ratingFilter/:rating',serves.getStocksWithRatingFilter);
+router.get('/menuitems/priceFilter/:price',serves.getStocksWithPriceFilter);
+router.get('/menuitems/TimesOrderedFilter/:times',serves.getStocksWithTimesOrderedFilter);
+//Bookings Routes
+router.get('/booings/TableNumberFilter/:TableNumber',serves.getBookingWithTableNumberFilter);
+router.get('/booings/NumberOfPeopleFilter/:NumberOfPeople',serves.getBookingWithNumberOfPeopleFilter);
 
 module.exports = router;
