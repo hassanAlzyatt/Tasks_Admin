d4f056146ea38d20f4acaa9fd2f19499fe97b710
hassanAlzyatt <152031047+hassanAlzyatt@users.noreply.github.com>
2023-11-30 20:59:05 +0200

    Update routers.js

--- adminTasks/routers.js
+++ adminTasks/routers.js
@@ -10,9 +10,8 @@ router.put('/admin/updateItem/:itemId', serves.updateRecord)
 router.delete('/admin/deleteMenuItem/:itemId', serves.deleteRecord)
 
 // Stock Routes 
-router.get('/menuitems/ratingFilter/:rating',serves.getStocksWithRatingFilter);
-router.get('/menuitems/priceFilter/:price',serves.getStocksWithPriceFilter);
-router.get('/menuitems/TimesOrderedFilter/:times',serves.getStocksWithTimesOrderedFilter);
+router.get('/menuitems/getStockWithFilter/',serves.getStocksWithFilter);
+
 //Bookings Routes
 router.get('/booings/TableNumberFilter/:TableNumber',serves.getBookingWithTableNumberFilter);
 router.get('/booings/NumberOfPeopleFilter/:NumberOfPeople',serves.getBookingWithNumberOfPeopleFilter);
